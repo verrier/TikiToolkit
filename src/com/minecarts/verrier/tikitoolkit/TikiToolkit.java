5830cad24b8216a217c988c33ebc334067557600
Stephen <stephen@jazer.com>
2011-03-01 21:16:42 -0500

    Added major functionality, works basically

--- src/com/minecarts/verrier/tikitoolkit/TikiToolkit.java
+++ src/com/minecarts/verrier/tikitoolkit/TikiToolkit.java
@@ -1,11 +1,19 @@
 package com.minecarts.verrier.tikitoolkit;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.util.config.Configuration;
+
+import java.util.HashMap;
 
 import com.minecarts.verrier.tikitoolkit.listener.*;
 
@@ -14,6 +22,11 @@ public class TikiToolkit extends JavaPlugin{
 	private PlayerListener playerListener;
 	private EntityListener entityListener;
 	
+	public HashMap<String,Integer> playerSlot;
+	public Configuration config;
+
+	public final Logger log = Logger.getLogger("Minecraft");
+	
 	public void onEnable() {
 		PluginManager pm = getServer().getPluginManager();
 		
@@ -21,18 +34,34 @@ public class TikiToolkit extends JavaPlugin{
 		playerListener = new PlayerListener(this);
 		entityListener = new EntityListener(this);
 		
+		playerSlot = new HashMap<String,Integer>();
+		
 		pm.registerEvent(Type.PLAYER_ANIMATION, this.playerListener, Event.Priority.Monitor, this);
 		pm.registerEvent(Type.PLAYER_ITEM, this.playerListener, Event.Priority.Monitor, this);
 		pm.registerEvent(Type.PLAYER_ITEM_HELD, this.playerListener, Event.Priority.Monitor, this);
 		pm.registerEvent(Type.PLAYER_RESPAWN, this.playerListener, Event.Priority.Monitor, this);
 		
 		pm.registerEvent(Type.ENTITY_DEATH, this.entityListener, Event.Priority.Monitor, this);
+		
+		this.config = getConfiguration();
+		//saveConfiguration();
+	
+		PluginDescriptionFile pdf = getDescription();
+	    this.log.info("[" + pdf.getName() + "] version " + pdf.getVersion() + " loaded.");
 	}
 	
 	public void onDisable() {
 		
 	}
 	 
+	private void saveConfiguration(){
+		   	this.config.setProperty("admins.verrier.wand.type","STICK");
+		   	this.config.setProperty("admins.verrier.wand.slot_1.name","Test Tool");
+		   	this.config.setProperty("admins.verrier.wand.slot_1.click_left","/hpos1");
+		   	this.config.setProperty("admins.verrier.wand.slot_1.click_right","/hpos2");
+		   	this.config.save();
+	   }
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args){
 		return false;
 	}
--- src/com/minecarts/verrier/tikitoolkit/listener/PlayerListener.java
+++ src/com/minecarts/verrier/tikitoolkit/listener/PlayerListener.java
@@ -1,9 +1,10 @@
 package com.minecarts.verrier.tikitoolkit.listener;
 
 import com.minecarts.verrier.tikitoolkit.*;
-import org.bukkit.event.player.*;
-import org.bukkit.event.entity.EntityDeathEvent;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.player.*;
 import org.bukkit.entity.Player;
 
 public class PlayerListener extends org.bukkit.event.player.PlayerListener{
@@ -18,19 +19,49 @@ public class PlayerListener extends org.bukkit.event.player.PlayerListener{
 	
 	
 	public void onPlayerAnimation(PlayerAnimationEvent event) {
-		
+		if (event.getAnimationType() == PlayerAnimationType.ARM_SWING) {
+			this.doWandCmd(event.getPlayer(),"click_left");
+		}
 	}
 	
 	public void onPlayerItem(PlayerItemEvent event){
-		
+		this.doWandCmd(event.getPlayer(),"click_right");
 	}
 	
-	public void onPlayerItemHeld(PlayerItemHeldEvent event){
+	public void onItemHeldChange(PlayerItemHeldEvent event){
 		Player player = event.getPlayer();
-		player.sendMessage(String.format("New inventory slot: %s",event.getNewSlot()));
+		int slot = event.getNewSlot();
+		
+		//Store our new (current) slot
+		plugin.playerSlot.put(player.getName(), slot);
+		String name = plugin.config.getString("admins."+player.getName()+".slot_"+slot+".name");
+		if(name != null){
+			player.sendMessage(String.format("Tiki: %s %s %s selected",ChatColor.GOLD,name,ChatColor.WHITE));
+		}
 	}
 	
 	public void onPlayerRespawn(PlayerRespawnEvent event){
 		
-	}	
+	}
+	
+	
+	
+	private void doWandCmd(Player player, String clickType){
+		if(!plugin.playerSlot.containsKey(player.getName())){
+			player.sendMessage("Please select cycle your wands so we can start tracking them");
+			return;
+		}
+		
+		int slot = plugin.playerSlot.get(player.getName());
+		String type = plugin.config.getString("admins."+player.getName()+".slot_"+slot+".type","STICK");
+		
+		//Check to see if the item in the hand is their configured wand
+		if (player.getInventory().getItemInHand().getType() == Material.getMaterial(type)){
+			//Execute the command
+			String cmd = plugin.config.getString("admins."+player.getName()+".slot_"+slot+"."+clickType);
+			if(cmd!= null){
+				player.performCommand(cmd);
+			}
+		}
+	}
 }
