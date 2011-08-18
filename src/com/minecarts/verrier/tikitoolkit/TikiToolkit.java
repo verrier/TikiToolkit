274b9ffb3bb1ca5a4896c007a910ef9a60f25067
Stephen <stephen@jazer.com>
2011-08-17 22:08:19 -0400

    Added initial in game commands to bind to tools

--- src/com/minecarts/verrier/tikitoolkit/TikiToolkit.java
+++ src/com/minecarts/verrier/tikitoolkit/TikiToolkit.java
@@ -1,8 +1,11 @@
 package com.minecarts.verrier.tikitoolkit;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
+import com.minecarts.verrier.tikitoolkit.helper.StringHelper;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -57,8 +60,9 @@ public class TikiToolkit extends JavaPlugin{
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args){
 		if(cmdLabel.toLowerCase().equals("tiki")){
-			if(args.length == 1){
-				if(args[0].toLowerCase().equals("reload")){
+			if(args.length > 0){
+                //Reload the config
+				if(args[0].equalsIgnoreCase("reload")){
 					//If a player issued the command
 					if(sender instanceof Player){
 						//Check to see if they're an op
@@ -72,15 +76,15 @@ public class TikiToolkit extends JavaPlugin{
 					}
 					log.info("TikiToolkit config reloaded.");
 					return true;
-				} else 
-				if(args[0].toLowerCase().equals("identify")){
+                //Identify what item you have in hand
+				} else if(args[0].equalsIgnoreCase("identify")){
 					if(sender instanceof Player){
 						Player player = (Player) sender;
 						player.sendMessage("Tiki: You are holding: " + ChatColor.GOLD + player.getInventory().getItemInHand().getType().name());
 						return true;
-					}
-				} else 
-				if(args[0].toLowerCase().equals("tools")){
+				}
+                //Give the player their tools
+				} else if(args[0].equalsIgnoreCase("tools")){
 						if(sender instanceof Player){
 							Player player = (Player) sender;
 							
@@ -90,9 +94,29 @@ public class TikiToolkit extends JavaPlugin{
 							getServer().getScheduler().scheduleSyncDelayedTask(this, setInventory,1);
 							return true;
 						}
-					}
-			}
+				} else if(args[0].equalsIgnoreCase("bind")){
+                    if(args.length > 2){
+                        if(sender instanceof Player){
+							Player player = (Player) sender;
+                            config.setProperty("admins."+player.getName()+".slot_"+player.getInventory().getHeldItemSlot()+".type",player.getItemInHand().getType().toString());
+
+                            if(args[1].equalsIgnoreCase("click_left") || args[1].equalsIgnoreCase("click_right")){
+                                //Check to see if there are multiple args after this, and if so, try to figure out how to break it apart
+                                //  for now, we're just going to assume it's a single command (eg no chaining (yet))
+
+                                String combinedArgs = StringHelper.join(args,2);
+
+                                config.setProperty("admins."+player.getName()+".slot_"+player.getInventory().getHeldItemSlot()+"." + args[1].toLowerCase(),combinedArgs);
+                            } else {
+                                player.sendMessage("Please use click_left or click_right to define the click action.");
+                            }
+                            config.save();
+                            return true;
+                        }
+                    }
+                }
+            }
 		}
 		return false;
-	}
+    }
 }
